593539c03eee8647988405d5ad46bc2b30ce91b2
A1exander-liU <alexanderliu2016@gmail.com>
2024-07-26 15:50:22 -0700

    feat: create navbar

---
+++ src/components/navbar.tsx
@@ -0,0 +1,31 @@
+import { Link } from "gatsby";
+import React from "react";
+import ThemeToggle from "./themeToggle";
+export default function Navbar() {
+  return (
+    <nav className="z-10 px-2 h-12 w-full top-0 sticky space-x-2 flex items-center shadow-lg bg-slate-100 dark:bg-slate-900 dark:text-white">
+      <Link
+        to="/#home"
+        className="px-2 py-1 hover:bg-slate-200 dark:hover:bg-slate-700 rounded-sm font-medium"
+      >
+        Home
+      </Link>
+      <Link
+        to="/#about"
+        className="px-2 py-1 hover:bg-slate-200 dark:hover:bg-slate-700 rounded-sm font-medium"
+      >
+        About
+      </Link>
+      <Link
+        to="/#contact"
+        className="px-2 py-1 hover:bg-slate-200 dark:hover:bg-slate-700 rounded-sm font-medium"
+      >
+        Contact
+      </Link>
+
+      <span className="flex-1"></span>
+
+      <ThemeToggle />
+    </nav>
+  );
+}
