e8ac899f88f5e8c9c8b0a7b3bf1142a1be0efafd
A1exander-liU <alexanderliu2016@gmail.com>
2024-07-26 15:50:12 -0700

    feat: create dropdown for theme selection

---
+++ src/components/themeToggle.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/react";
+import { toTitleCase } from "../scripts/stringUtils";
+import { Theme, changeTheme, themes } from "../scripts/theme";
+import { SunIcon } from "@heroicons/react/24/solid";
+
+export default function ThemeToggle() {
+  const initialTheme = "theme" in localStorage ? localStorage.theme : "system";
+  const [currentTheme, setCurrentTheme] = useState<Theme>(initialTheme);
+
+  const handleThemeChange = async (theme: Theme) => {
+    changeTheme(theme);
+    setCurrentTheme(theme);
+  };
+
+  return (
+    <>
+      <Menu>
+        <MenuButton className="px-2 py-1 rounded-full data-[hover]:bg-slate-200 dark:data-[hover]:bg-slate-700">
+          <SunIcon className="size-6" />
+        </MenuButton>
+        <MenuItems
+          anchor="bottom end"
+          transition
+          className="z-20 p-1 rounded-md bg-slate-50 dark:bg-slate-900 dark:text-white ring-2 ring-opacity-10 ring-black dark:ring-gray-700 origin-top transition duration-200 ease-out data-[closed]:scale-95 data-[closed]:opacity-0"
+        >
+          {themes.map((theme, i) => (
+            <MenuItem key={i}>
+              <p
+                onClick={() => handleThemeChange(theme)}
+                className={`px-4 py-1 rounded-md data-[focus]:bg-slate-200 dark:data-[focus]:bg-slate-700 ${currentTheme === theme ? "text-blue-400" : ""} text-sm font-bold cursor-pointer`}
+              >
+                {toTitleCase(theme)}
+              </p>
+            </MenuItem>
+          ))}
+        </MenuItems>
+      </Menu>
+    </>
+  );
+}
