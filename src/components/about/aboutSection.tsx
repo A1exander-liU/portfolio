98eedc154e5693177d9bea78820e088c92e0a31f
A1exander-liU <alexanderliu2016@gmail.com>
2024-08-06 10:39:10 -0700

    feat: craete about section component

---
+++ src/components/about/aboutSection.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { motion } from "framer-motion";
+import SkillsSection from "./skillsSection";
+
+
+
+export default function AboutSection() {
+  return (
+    <div id="about" className="pt-20">
+      <motion.p
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 0.75 }}
+        viewport={{ once: true }}
+        className="mb-20 text-center">
+        Random Random Random Random Random Random Random Random Random Random
+        Random Random Random Random Random Random Random Random Random Random
+        Random Random Random Random Random Random Random Random Random Random
+        Random Random Random Random Random Random Random Random Random Random
+        Random Random Random Random Random Random Random Random Random Random
+        Random Random Random Random Random Random Random Random Random Random
+        Random Random Random Random Random Random Random Random Random Random
+      </motion.p>
+
+      <SkillsSection />
+
+    </div>
+  )
+}
