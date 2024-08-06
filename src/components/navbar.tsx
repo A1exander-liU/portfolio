aa748737b0686a0e862b569a260fe0b9b08949c4
A1exander-liU <alexanderliu2016@gmail.com>
2024-08-06 10:39:34 -0700

    feat: update navbar links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,11 +1,12 @@
 import { Link } from "gatsby";
 import React from "react";
 import ThemeToggle from "./themeToggle";
+
 export default function Navbar() {
   return (
     <nav className="z-10 px-2 h-12 w-full top-0 sticky space-x-2 flex items-center shadow-lg bg-slate-100 dark:bg-slate-900 dark:text-white">
       <Link
-        to="/#home"
+        to="/"
         className="px-2 py-1 hover:bg-slate-200 dark:hover:bg-slate-700 rounded-sm font-medium"
       >
         Home
@@ -17,10 +18,10 @@ export default function Navbar() {
         About
       </Link>
       <Link
-        to="/#contact"
+        to="/#projects"
         className="px-2 py-1 hover:bg-slate-200 dark:hover:bg-slate-700 rounded-sm font-medium"
       >
-        Contact
+        Projects
       </Link>
 
       <span className="flex-1"></span>
