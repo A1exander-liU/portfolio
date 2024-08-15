9f41b062eb32763fd88812f03337c7b32753af99
A1exander-liU <alexanderliu2016@gmail.com>
2024-08-15 11:28:29 -0700

    feat: alternate card positions on larger screens

--- src/components/projectCard.tsx
+++ src/components/projectCard.tsx
@@ -1,27 +1,27 @@
 import React from "react"
 import { motion } from "framer-motion"
 import { Node } from "../pages"
-import { GatsbyImage, StaticImage, getImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { CodeBracketIcon, GlobeAltIcon } from "@heroicons/react/24/solid"
 
 
-export default function ProjectCard({ node }: { node: Node }) {
+export default function ProjectCard({ node, pos }: { node: Node, pos: "left" | "right" }) {
   const imageData = getImage(node.frontmatter.hero_image)
 
   return (
     <motion.div
-      className="px-2 py-1 min-h-64 flex flex-col justify-between"
+      className={`px-2 py-1 min-h-64 w-full lg:max-w-[60%] flex flex-col justify-between md:${pos === "left" ? "self-start" : "self-end"}`}
     >
 
       <h1 className="font-medium text-lg">{node.frontmatter.title}</h1>
 
       <div className="mb-5 flex justify-center items-center">{imageData && <GatsbyImage image={imageData} alt="icon" objectFit="contain" className="max-h-24" />}</div>
 
-      <p className="my-4">{node.excerpt}</p>
+      <p className="my-4">{node.frontmatter.info}</p>
 
       <div className="flex justify-end space-x-4">
         <a href={node.frontmatter.live} className="w-1/3 max-w-32">
-          <motion.button className="px-4 py-2 w-full rounded-full bg-blue-500 text-black dark:text-white">
+          <motion.button className="px-4 py-2 w-full rounded-full bg-blue-500 text-black dark:text-white hover:brightness-110">
             <p className="flex justify-center space-x-2"><span>Live</span> <GlobeAltIcon className="w-6" /></p>
           </motion.button>
         </a>
@@ -31,6 +31,6 @@ export default function ProjectCard({ node }: { node: Node }) {
           </motion.button>
         </a>
       </div>
-    </motion.div>
+    </motion.div >
   )
 }
