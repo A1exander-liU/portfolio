51d9145714d954f84d43b1c20629ada4594192e8
A1exander-liU <alexanderliu2016@gmail.com>
2024-08-20 16:26:42 -0700

    feat: adjust sizing in contact section

--- app/components/contactSection.tsx
+++ app/components/contactSection.tsx
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function ContactSection() {
   return (
-    <section id="contact" className="py-1 mt-20 h-24 flex justify-center items-center bg-slate-800 w-full space-x-6">
+    <section id="contact" className="py-1 mt-20 h-20 flex justify-center items-center bg-slate-800 w-full space-x-6">
       <a href="https://github.com/A1exander-liU">
-        <i className="devicon-github-original text-white text-4xl"></i>
+        <i className="devicon-github-original text-white text-2xl"></i>
       </a>
       <a href="https://linkedin.com/in/alexanderliu00">
-        <i className="devicon-linkedin-plain colored text-4xl"></i>
+        <i className="devicon-linkedin-plain colored text-2xl"></i>
       </a>
     </section>
   );
