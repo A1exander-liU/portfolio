b75cd00e84c1756ee86ca8181e9874486a799c2d
A1exander-liU <alexanderliu2016@gmail.com>
2024-08-19 21:49:31 -0700

    feat: update tech bar
    add show more icon to display overflowed items

--- app/components/project/projectCard2.tsx
+++ app/components/project/projectCard2.tsx
@@ -1,12 +1,78 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { motion } from "framer-motion";
 import { ProjectNode } from "../../scripts/types";
 import Image from "next/image";
+import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid";
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+
+function ShowMore({ hidden }: { hidden: React.JSX.Element[] }) {
+  return (
+    <Menu>
+      <MenuButton>
+        <EllipsisHorizontalIcon className="w-6" />
+      </MenuButton>
+      <MenuItems
+        anchor="bottom end"
+        transition
+        className="z-20 max-w-10 p-1 flex flex-wrap items-center space-x-1 rounded-md bg-slate-50 dark:bg-slate-900 dark:text-white ring-2 ring-opacity-10 ring-black dark:ring-gray-700 origin-top transition duration-200 ease-out data-[closed]:scale-95 data-[closed]:opacity-0"
+      >
+        {hidden.map((node, i) => (
+          <MenuItem key={i}>{node}</MenuItem>
+        ))}
+      </MenuItems>
+    </Menu>
+  );
+}
+
+function TechBar({ tech, pos }: { tech: string[]; pos: string }) {
+  const allChildren = tech.map((name, i) => <i key={i} className={`devicon-${name}-plain text-lg`}></i>);
+  const techBarRef = useRef<HTMLDivElement | null>(null);
+
+  const [displayedChildren, setDisplayedChildren] = useState<React.JSX.Element[]>([]);
+  const [hiddenChildren, setHiddenChildren] = useState<React.JSX.Element[]>([]);
+
+  // Sizes (pixels)
+  const spacing = 4; // space-x-1
+  const iconSize = 18; // text-lg
+  const showMoreIconSize = 24 + spacing;
+
+  const handleOverflow = () => {
+    const container = techBarRef.current;
+    if (!container) return;
+
+    const containerWidth = container.clientWidth - showMoreIconSize;
+    const childWidth = iconSize + spacing;
+    const slots = Math.floor(containerWidth / childWidth);
+
+    setDisplayedChildren(allChildren.slice(0, slots));
+    setHiddenChildren(allChildren.slice(slots));
+  };
+
+  useEffect(() => {
+    console.log(hiddenChildren);
+  }, [hiddenChildren]);
+
+  useEffect(() => {
+    handleOverflow();
+    window.addEventListener("resize", handleOverflow);
+    return () => window.removeEventListener("resize", handleOverflow);
+  }, []);
+
+  return (
+    <div
+      ref={techBarRef}
+      className={`flex overflow-hidden justify-end items-center space-x-1 ${pos === "right" ? "md:col-start-1 md:row-start-1 md:justify-start" : ""}`}
+    >
+      {displayedChildren}
+      {hiddenChildren.length > 0 && <ShowMore hidden={hiddenChildren} />}
+    </div>
+  );
+}
 
 export default function ProjectCard2({ node, pos }: { node: ProjectNode; pos: "left" | "right" }) {
   return (
     <motion.div
-      className={`p-2 min-h-64 w-full grid grid-cols-1 md:grid-cols-2 gap-2 lg:max-w-[65%] text-sm md:text-base dark:bg-slate-800 ${pos === "left" ? "self-start" : "self-end"} shadow-lg`}
+      className={`p-2 min-h-64 w-full grid grid-cols-1 md:grid-cols-2 gap-2 border-x-2 border-t-2 border-solid border-slate-200 dark:border-slate-700 dark:border-2 lg:max-w-[65%] text-sm md:text-base dark:bg-slate-800 ${pos === "left" ? "self-start" : "self-end"} shadow-lg`}
     >
       <div
         className={`relative p-1 grid md:block grid-rows-subgrid row-start-1 col-start-1 ${pos === "left" ? "md:col-start-1" : "md:col-start-2"} row-span-2`}
@@ -50,15 +116,25 @@ export default function ProjectCard2({ node, pos }: { node: ProjectNode; pos: "l
             <p className="flex justify-center space-x-2 font-semibold">Read More</p>
           </motion.a>
         </div>
-        <div
-          className={`flex overflow-hidden justify-end items-center space-x-1 ${pos === "right" ? "md:col-start-1 md:row-start-1 md:justify-start" : ""}`}
-        >
-          <i className="devicon-javascript-plain text-lg"></i>
-          <i className="devicon-materialui-plain text-lg"></i>
-          <i className="devicon-react-plain text-lg"></i>
-          <i className="devicon-css3-plain text-lg"></i>
-          <i className="devicon-html5-plain text-lg"></i>
-        </div>
+        <TechBar
+          tech={[
+            "javascript",
+            "materialui",
+            "react",
+            "html5",
+            "css3",
+            "css3",
+            "css3",
+            "css3",
+            "react",
+            "html5",
+            "css3",
+            "css3",
+            "css3",
+            "css3",
+          ]}
+          pos={pos}
+        />
       </div>
     </motion.div>
   );
