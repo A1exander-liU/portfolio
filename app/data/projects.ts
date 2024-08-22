44be0c017d01cc737562f7d4a36a13442177f907
A1exander-liU <alexanderliu2016@gmail.com>
2024-08-22 16:23:22 -0700

    feat: update project info

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -2,9 +2,9 @@ import { ProjectNode } from "../scripts/types";
 
 const pokestore: ProjectNode = {
   title: "PokeStore",
-  live: "https://mini-ebon.vercel.app",
+  live: "https://mini-poke-store.vercel.app",
   source: "https://github.com/A1exander-liU/mini/tree/main/e-commerce",
-  hero_image: "/project_images/github-profile-searcher.png",
+  hero_image: "/project_images/pokestore.png",
   hero_image_alt: "gatsby icon",
   info: "Fake e-commerce store for purchasing pokemon related items.",
   tech: [
@@ -17,9 +17,9 @@ const pokestore: ProjectNode = {
 
 const interviewApp: ProjectNode = {
   title: "AI-Powered Interview App",
-  live: "https://mini-ebon.vercel.app",
+  live: "",
   source: "https://github.com/mScrub/Drinking-Wine---Hackathon",
-  hero_image: "/project_images/github-profile-searcher.png",
+  hero_image: "/project_images/ai-interview-app.png",
   hero_image_alt: "gatsby icon",
   info: "Interview practice app with AI interviewer utilizing text-to-speech, speech-to-text and ChatGPT",
   tech: [
