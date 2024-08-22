1f6473a2af0130b5b3e775f10970a5e4f203b276
A1exander-liU <alexanderliu2016@gmail.com>
2024-08-22 14:39:48 -0700

    feat: replace dummy data

--- app/components/about/aboutSection.tsx
+++ app/components/about/aboutSection.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { motion } from "framer-motion";
 import SkillsSection from "./skillsSection";
+import { info } from "@/app/data/info";
 
 export default function AboutSection() {
   return (
-    <section id="about" className="pt-20 px-10 lg:flex lg:justify-center lg:space-x-10">
-      <motion.div className="mb-10 lg:w-1/2">
-        Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random
-        Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random
-        Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random
-        Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random Random
-        Random Random Random Random Random Random
-      </motion.div>
-
-      <SkillsSection />
-    </section>
+    <>
+      <h1 id="about" className="flex flex-col items-center font-bold text-4xl mb-8 pt-14 md:text-5xl">
+        About
+        <hr className="mt-1 w-3/5 border-2 border-blue-500" />
+      </h1>
+      <section className="px-10 flex flex-col xl:flex-row items-center xl:justify-center lg:space-x-10 xl:items-start">
+        <motion.div className="mb-10 md:text-xl xl:w-1/2">{info.about}</motion.div>
+        <SkillsSection />
+      </section>
+    </>
   );
 }
--- app/data/info.ts
+++ app/data/info.ts
@@ -1,3 +1,5 @@
 export const info = {
   name: "Alexander Liu",
+  about:
+    "My preference to learn on the go enables me to quickly adapt to new technologies. I have a strong focus and experience in everything web development from creating sites to developing api servers. When creating and designing systems, my priority is to make them easy to use for both the end users and developers.",
 };
