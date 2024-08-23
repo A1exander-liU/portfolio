ca5d594fc79d8cd6e34a7ec4fde29f523ced5de8
A1exander-liU <alexanderliu2016@gmail.com>
2024-08-23 12:24:08 -0700

    feat: add more info

--- app/data/info.ts
+++ app/data/info.ts
@@ -1,5 +1,6 @@
+/* Update info, need more stuff here */
 export const info = {
   name: "Alexander Liu",
   about:
-    "My preference to learn on the go enables me to quickly adapt to new technologies. I have a strong focus and experience in everything web development from creating sites to developing api servers. When creating and designing systems, my priority is to make them easy to use for both the end users and developers.",
+    "Fully committed to learning on the go, I'm a developer that enjoys trying new things to gain practical experience which enables me to quickly adapt to new technologies. I also have a strong focus and experience in everything web development from creating sites to developing api servers. When creating software, I aim to make it easy to not only for users to use but for developers as well by writing clean and well orgainized code.",
 };
